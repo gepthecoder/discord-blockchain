d75d8e6846ac0c41bd0f9e33871c857eeb888ca9
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-25 15:07:46 +0200

    $0018: [Home] [ConversationList]: create conversation list component, add css

---
+++ client/components/ConversationList.js
@@ -0,0 +1,11 @@
+import styles from '../styles/conversationList.module.css'
+
+const ConversationList = () => {
+  return (
+    <div className={styles.conversations}>
+      
+    </div>
+  )
+}
+
+export default ConversationList
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,13 @@
 import styles from '../styles/Home.module.css'
 import Sidebar from '../components/Sidebar'
+import ConversationList from '../components/ConversationList'
 
 export default function Home() {
   return (
     <div className={styles.wrapper}>
       <Sidebar />
       <div className={styles.main}>
-        {/* conversationList */}
+        <ConversationList />
         {/* chat view */}
       </div>
     </div>
