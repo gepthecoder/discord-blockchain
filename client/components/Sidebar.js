0688519313fb00680b06c661e0e804b2f09a9b27
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-25 14:01:14 +0200

    $0014: [Home] [Sidebar]: create dummy data for channels

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,11 +1,44 @@
 import styles from '../styles/sidebar.module.css'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+import avatar1 from '../assets/avatar-1.webp'
+import avatar2 from '../assets/avatar-2.png'
+import avatar3 from '../assets/avatar-3.webp'
+import avatar4 from '../assets/avatar-4.webp'
+
+const dummyChannels = [
+    {
+        id: 1,
+        name: 'general',
+        avatar: avatar1,
+    },
+    {
+        id: 2,
+        name: 'general',
+        avatar: avatar2,
+    },
+    {
+        id: 3,
+        name: 'general',
+        avatar: avatar3,
+    },
+    {
+        id: 4,
+        name: 'general',
+        avatar: avatar4,
+    },
+]
 
 const Sidebar = () => {
-  return (
-    <div className={styles.wrapper}>
-        Sidebar
-    </div>
-  )
+    const router = useRouter()
+    const [channels, setChannels] = useState([])
+
+    return (
+        <div className={styles.wrapper}>
+            Sidebar
+        </div>
+    )
 }
 
 export default Sidebar
