b26926f307ecb76ca48dd6d5a05cbc54dc04f9db
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-25 23:15:31 +0200

    $0037: [Sidebar]: remove dummy channels data

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -2,39 +2,12 @@ import styles from '../styles/sidebar.module.css'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
-import avatar1 from '../assets/avatar-1.webp'
-import avatar2 from '../assets/avatar-2.png'
-import avatar3 from '../assets/avatar-3.webp'
-import avatar4 from '../assets/avatar-4.webp'
-
 import RoomAvatar from './RoomAvatar'
 
-const dummyChannels = [
-    {
-        roomId: 1,
-        roomName: 'general',
-        avatar: avatar1,
-    },
-    {
-        roomId: 2,
-        roomName: 'random',
-        avatar: avatar2,
-    },
-    {
-        roomId: 3,
-        roomName: 'chill',
-        avatar: avatar3,
-    },
-    {
-        roomId: 4,
-        roomName: 'buildspace',
-        avatar: avatar4,
-    },
-]
 
 const Sidebar = () => {
     const router = useRouter()
-    const [channels, setChannels] = useState(dummyChannels)
+    const [channels, setChannels] = useState([])
 
     useEffect(() => {
         fetchChannels()
