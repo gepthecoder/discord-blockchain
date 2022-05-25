5a6a079de35d16e6b483dd731ba8609df91caa0e
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-25 23:53:41 +0200

    $0039: [MessageCard] [ChatView]: show messages in context with the state, add message card component

--- client/components/ChatView.js
+++ client/components/ChatView.js
@@ -1,14 +1,44 @@
 import styles from '../styles/chatView.module.css'
 import ChatHeader from './ChatHeader'
 import MessageForm from './MessageForm'
+import MessageCard from './MessageCard'
+
+import { useContext } from 'react'
+import { DiscordContext } from '../context/context'
 
 const ChatView = () => {
+    const { state } = useContext(DiscordContext)
+
+    /* Get All the messages from the state -> filter them -> bring the ones that bellong to the selected chat   */
+    const formattedMessagesArray = () => {
+        const uniqueArray = state.messages.filter((value, index) => {
+          const _value = JSON.stringify(value)
+    
+          return (
+            index ===
+            state.messages.findIndex(obj => {
+              return JSON.stringify(obj) === _value
+            })
+          )
+        })
+    
+        return uniqueArray
+    }
     return (
         <div className={styles.chatView}>
             <ChatHeader />
             <div className={styles.messagesContainer}>
-                <MessageForm />
+            {formattedMessagesArray().map((message, index) => (
+                <MessageCard
+                    key={index}
+                    avatar={message.avatar}
+                    sender={message.sender}
+                    timestamp={message.createdAt}
+                    content={message.content}
+                />
+            ))}
             </div>
+            <MessageForm />
         </div>
     )
 }
