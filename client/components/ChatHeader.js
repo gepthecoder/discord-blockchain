f79822d38c0b1b2b979c5af8f17e63538f1a0087
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-25 19:41:36 +0200

    $0033: [DiscordProvider [ChatHeader]]: bring in context values

--- client/components/ChatHeader.js
+++ client/components/ChatHeader.js
@@ -12,21 +12,24 @@ import at from '../assets/icons/at.svg'
 import styles from '../styles/chatHeader.module.css'
 import ethLogo from '../assets/eth.png'
 
-const currentAccount = '0xe276941FBd5f936E677dB9B6eEE8212a3b268C5E'
+import { DiscordContext } from '../context/context'
+import { useContext } from 'react'
 
 const ChatHeader = () => {
+    const { roomName, currentAccount, connectWallet } = useContext(DiscordContext)
+
     return (
         <div className={styles.chatHeader}>
             <div className={styles.roomNameContainer}>
                 <Image height={20} width={20} src={at} className={styles.svg} alt='' />
-                <h3 className={styles.title}>Room Name</h3>
+                <h3 className={styles.title}>{roomName}</h3>
                 <div className={styles.chatHeaderStatus} id='online' />
             </div>
             {currentAccount ? (
                 <div className={styles.connectedWallet}>
-                <Image src={ethLogo} height={20} width={15} alt='ethLogo' />
-                <span className={styles.separator}>{'|'}</span>
-                {currentAccount.slice(0, 6)}...{currentAccount.slice(39)}
+                    <Image src={ethLogo} height={20} width={15} alt='ethLogo' />
+                    <span className={styles.separator}>{'|'}</span>
+                    {currentAccount.slice(0, 6)}...{currentAccount.slice(39)}
                 </div>
             ) : (
                 <div className={styles.connectWallet} onClick={() => connectWallet()}>
@@ -35,22 +38,22 @@ const ChatHeader = () => {
             )}
             <div className={styles.headerIconsContainer}>
                 <div className={styles.headerItem}>
-                <Image
-                    height={25}
-                    width={25}
-                    src={phone}
-                    className={styles.svg}
-                    alt=''
-                />
+                    <Image
+                        height={25}
+                        width={25}
+                        src={phone}
+                        className={styles.svg}
+                        alt=''
+                    />
                 </div>
                 <div className={styles.headerItem}>
-                <Image
-                    height={25}
-                    width={25}
-                    src={video}
-                    className={styles.svg}
-                    alt=''
-                />
+                    <Image
+                        height={25}
+                        width={25}
+                        src={video}
+                        className={styles.svg}
+                        alt=''
+                    />
                 </div>
             </div>
             <div className={styles.headerItem}>
@@ -58,10 +61,10 @@ const ChatHeader = () => {
             </div>
             <div className={styles.headerItem}>
                 <Image
-                height={25}
-                width={25}
-                src={personPlus}
-                className={styles.svg}
+                    height={25}
+                    width={25}
+                    src={personPlus}
+                    className={styles.svg}
                 alt=''
                 />
             </div>
@@ -70,20 +73,20 @@ const ChatHeader = () => {
             </div>
             <div className={styles.headerItem}>
                 <Image
-                height={25}
-                width={25}
-                src={inbox}
-                className={styles.svg}
-                alt=''
+                    height={25}
+                    width={25}
+                    src={inbox}
+                    className={styles.svg}
+                    alt=''
                 />
             </div>
             <div className={styles.headerItem}>
                 <Image
-                height={25}
-                width={25}
-                src={help}
-                className={styles.svg}
-                alt=''
+                    height={25}
+                    width={25}
+                    src={help}
+                    className={styles.svg}
+                    alt=''
                 />
             </div>
         </div>
