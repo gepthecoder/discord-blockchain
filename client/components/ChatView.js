4c85f3cbf92a32336eb73960c125117a70bd4ced
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-25 16:51:29 +0200

    $0023 [Home] [ChatHeader] [ChatView]: create and style chat header with dummy auth

--- client/components/ChatView.js
+++ client/components/ChatView.js
@@ -1,8 +1,10 @@
 import styles from '../styles/chatView.module.css'
+import ChatHeader from './ChatHeader'
 
 const ChatView = () => {
     return (
         <div className={styles.chatView}>
+            <ChatHeader />
             <div className={styles.messagesContainer}>
                 ChatView
             </div>
