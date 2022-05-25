a008ceb9fa1dab5eee80828676ffa7db02012d38
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-25 14:20:34 +0200

    $0016: [Home] [Sidebar] [RoomAvatar]: render dummy channel logos

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,12 +1,13 @@
 import styles from '../styles/sidebar.module.css'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-import RoomAvatar from './RoomAvatar'
 
 import avatar1 from '../assets/avatar-1.webp'
 import avatar2 from '../assets/avatar-2.png'
 import avatar3 from '../assets/avatar-3.webp'
 import avatar4 from '../assets/avatar-4.webp'
+
+import RoomAvatar from './RoomAvatar'
 
 const dummyChannels = [
     {
@@ -33,7 +34,7 @@ const dummyChannels = [
 
 const Sidebar = () => {
     const router = useRouter()
-    const [channels, setChannels] = useState([])
+    const [channels, setChannels] = useState(dummyChannels)
 
     return (
         <div className={styles.wrapper}>
