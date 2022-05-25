068b3e1a226ff158d9f780ea6076e6f8fddeac38
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-25 18:53:28 +0200

    $0030: [Sanity] [GroqApi]: implement: fetch all conversations - channels

---
+++ client/pages/api/getchannels.js
@@ -0,0 +1,20 @@
+import { client } from '../../lib/client'
+
+/* Fetch all conversations associated with a particular user */
+
+const query = `*[_type == "conversations" && isDm==false]{
+  roomId,
+  roomName,
+  "avatar": image.asset->url
+}`
+
+export default async (req, res) => {
+    try {
+        const sanityResponse = await client.fetch(query)
+
+        res.status(200).send(sanityResponse)
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error)
+    }
+}
