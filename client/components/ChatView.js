cbdca7adc4e0e7d3d7652d812f8f56a647c7aabc
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-25 16:41:34 +0200

    $0022: [Home] [DirectMessages] [ChatView]: create chat view component, add style

---
+++ client/components/ChatView.js
@@ -0,0 +1,13 @@
+import styles from '../styles/chatView.module.css'
+
+const ChatView = () => {
+    return (
+        <div className={styles.chatView}>
+            <div className={styles.messagesContainer}>
+                ChatView
+            </div>
+        </div>
+    )
+}
+
+export default ChatView
